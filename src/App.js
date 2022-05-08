ab0e5c1ac4ae6494f0656b23a3e5585a7f6fe4cc
Sharmila <sharmilabunga@gmail.com>
2022-05-08 02:35:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Music from "./Music.js";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Music/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Music.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import {Table} from 'react-bootstrap';
+import Player from "./Player";
+const Music = () => {
+  const [songData, setSongData] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://gist.githubusercontent.com/CervantesVive/3f85bf26672cf27fe1cd932ffcb7ecac/raw/4de50b351a62158083a97f3b950bd786d3ffd928/awesome-podcasts.json")
+      .then((res) => {
+        const apiData = res.data.podcasts;//res.podcasts
+        console.log(apiData);
+
+        setSongData(apiData);//Apidata
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="main-container col-lg-12 col-md-12">
+      <h1 className="col-lg-6">Welcome to Music Listeners </h1>
+      <div className="tableDisplay">
+        <div className="tableData col-lg-12 col-md-12">
+         
+
+          <Table responsive>
+            <thead>
+              <tr>
+         
+          
+                {Object.keys(songData ? songData[0] : "").map((key) => (
+                <th>{key.toUpperCase()}</th>
+              ))}
+
+              </tr>
+            </thead>
+            <tbody>
+          
+             
+              
+              {songData?.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td >{val.name}</td>
+                  <td >{val.description}</td>
+                  <td >{val.source}</td>
+                  <td ><Player url={val.audio}/></td>
+                  <td >
+                  <img src={val.image} className="imgSize"></img></td>
+                  <td >{val.title}</td>
+                </tr>
+              );
+            })}
+            
+              
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Music;
